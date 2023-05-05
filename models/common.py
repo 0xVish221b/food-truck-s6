d244e5100fa9bd8ad84093ff58bb4be8e6c4f13b
0xVish221b <vishal.dogra@generalassemb.ly>
2023-05-05 15:28:34 +1000

    Fixed Silly Mistake

--- models/common.py
+++ models/common.py
@@ -4,7 +4,7 @@ import os
 db_env_constant = "DATABASE_URL"
 
 def sql_read(query, parameters=[]):
-    connection = psycopg2.connect(db_env_constant)
+    connection = psycopg2.connect(os.getenv(db_env_constant))
     cursor = connection.cursor()
     cursor.execute(query, parameters)
     results = cursor.fetchall()
